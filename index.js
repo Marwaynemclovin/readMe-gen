8622b0f71e3909d08e1698b39c11c63ed8586aaf
Marwin Manlangit <marwin.m@hotmail.com>
2023-04-14 23:28:19 +1000

    problems with readMe answers being generated

--- index.js
+++ index.js
@@ -1,83 +1,110 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
+
 const questions = [
-    {
-        type: 'input',
-        message: "What is the title of your project?",
-        name: 'title',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A project title is required.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "Write a description of your project.",
-        name: 'description',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A project description is required.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "What was your motivation?",
-        name: 'descriptionMotivation',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A motivation is required.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "What problem does the project solve?",
-        name: 'descriptionSolve',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A solution is required.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "What did you learn?",
-        name: 'descriptionLearn',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("What was learnt required.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "Provide instructions and examples for use.",
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: "List your collaborators.",
-        name: 'credits'
-    },
-    {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['MIT License', 'The  Unlicense', 'Other'],
-        name: 'license'
+  {
+    type: 'input',
+    message: "What is the title of your project?",
+    name: 'title',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("A project title is required.");
+      }
+      return true;
     }
+  },
+  {
+    type: 'input',
+    message: "Write a description of your project.",
+    name: 'description',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("A project description is required.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "What was your motivation?",
+    name: 'descriptionMotivation',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("A motivation is required.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "What problem does the project solve?",
+    name: 'descriptionSolve',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("A solution is required.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "What did you learn?",
+    name: 'descriptionLearn',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("What was learnt required.");
+      }
+      return true;
+    }
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Provide a step-by-step description of how to get the development environment running.",
+  },
+  {
+    type: 'input',
+    message: "Provide instructions and examples for use.",
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: "List your collaborators.",
+    name: 'credits'
+  },
+  {
+    type: "input",
+    message: "Email for related questions to project.",
+    name: "email"
+  },
+  {
+    type: 'list',
+    message: "Choose a license for your project.",
+    choices: ['MIT License', 'The Unlicense', 'Apache License 2.0', 'GNU General Public License v3.0', 'Mozilla Public License 2.0', 'Other'],
+    name: 'license'
+  },
+  {
+    type: 'input',
+    message: "Please specify the license for your project.",
+    name: 'customLicense',
+    when: function(answers) {
+      return answers.license === 'Other';
+    }
+  }
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
+function writeToFile(fileName, userInputs) {
+  let markDown = generateMarkdown(userInputs);
+
+  let filePath = "./docs/" + fileName;
+
+  fs.writeFileSync(filePath, markDown, function () {
+    console.log("README.md has been generated.");
+  });
+}
+
 function writeToFile(fileName, userInputs) {
     let markDown = generateMarkdown(userInputs);
     console.log(markDown, fileName, userInputs, "hello");
@@ -86,24 +113,19 @@ function writeToFile(fileName, userInputs) {
       console.log("README.md has been generated.");
     });
   }
-// TODO: Create a function to initialize app
+
+// Function to initialize app
 function init() {
-    inquirer
-        .prompt(questions)
-        .then((answers) => {
-            const markdown = generateMarkdown(answers);
-            console.log("Answers and generated markdown:", answers, markdown);
-            writeToFile("readMe.md", markdown);
-        })
-        .catch((error) => {
-            if (error.isTtyErr) {
-                console.error("Prompt could not be rendered in the current environment. Try running this program in a terminal or command prompt instead.");
-            } else {
-                console.error("An error occurred while generating the readme file:", error);
-            }
-        });
+  inquirer.prompt(questions)
+    .then((answers) => {
+      let readMeData = generateMarkdown(answers);
+      writeToFile("README.md", readMeData);
+      console.log("README.md has been generated successfully.");
+    })
+    .catch((err) => {
+      console.log("An error occurred while generating the README file:", err);
+    });
 }
 
-
-// Function call to initialize app
+// Function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return ` # ${data.title}
+  
+  ## Description
+  ## Table Of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Questions](#questions)
+  * [Screenshot](#screenshot)
+  * [Video](#video)
+  
+  The motivation for this project was ${data.descriptionMotivation}. 
+    
+  The problem which needed to be solved was ${data.descriptionSolve}.
+  
+  From this project I learnt ${data.descriptionLearn}.
+  
+  ## Installation
+  To install this application ${data.installation}.
+  
+  ## Usage
+  The intended use of this application is ${data.usage}.
+  
+  
+  ## Credits
+  The development person/team responsible for this is ${data.credits}.
+  
+  ## Licence 
+  This project has a ${data.license}.
+  ## Questions 
+  If there are any questions about the project feel free to email me at:  
+  - Email: ${data.email}
+  
+  ## Screenshot
+  
+  ## Video 
+  `;
 }
 
 module.exports = generateMarkdown;
